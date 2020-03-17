43a4f61a945fb9fc5216508513e0094018b36863
Michael Schneider <mic.sch91@gmail.com>
2020-03-17 19:01:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,8 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == "" || position_taken?(board, index) == " "
     return true
+  if position_taken?(board, index).between?(0,8)
+    return true
   else
     return false
 
